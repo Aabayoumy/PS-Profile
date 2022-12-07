1e06cc653744ca3e4e87c1e68771a2cbdcdb62fc
Ahmed M. Bayoumy <abayoumy@outlook.com>
2022-12-07 07:54:28 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,11 @@
-if ((winget list --id "Starship.Starship").count -lt 5) {     winget install starship --force }
+# By :    ABayoumy@outlook.com
+# GitHub: https://github.com/Aabayoumy/PS-Profile
+
+Write-Host "Install starship https://starship.rs/ if not installed"
+if ((winget list --id "Starship.Starship").count -lt 5) { winget install starship --force }
 If (Test-Path -Path "$env:TEMP\Fonts") { Remove-Item -Recurse -Force $env:TEMP\Fonts }
 
+Write-Host "Download Fonts"
 ## Hack Fonts
 #Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip  -OutFile "$env:TEMP\Hack.zip"
 #Expand-Archive -LiteralPath "$env:TEMP\Hack.zip" -DestinationPath "$env:TEMP\Fonts\" -force
@@ -9,6 +14,7 @@ If (Test-Path -Path "$env:TEMP\Fonts") { Remove-Item -Recurse -Force $env:TEMP\F
 #Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip  -OutFile "$env:TEMP\JetBrainsMono.zip"
 #Expand-Archive -LiteralPath "$env:TEMP\JetBrainsMono.zip" -DestinationPath "$env:TEMP\Fonts\" -force
 
+## CascadiaCode Fonts
 Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/CascadiaCode.zip -OutFile "$env:TEMP\CascadiaCode.zip"
 Expand-Archive -LiteralPath "$env:TEMP\CascadiaCode.zip" -DestinationPath "$env:TEMP\Fonts\" -force
 Write-Host "Install fonts"
@@ -19,15 +25,19 @@ foreach ($file in Get-ChildItem "$env:TEMP\Fonts\*windows*.ttf")
     if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
         Write-Output $fileName
         Get-ChildItem $file | ForEach-Object{ $fonts.CopyHere($_.fullname,16) }
+
     }
 }
-Move-Item *.ttf c:\windows\fonts\
-# Move-Item "$env:TEMP\Fonts\*.ttf" "C:\Windows\Fonts\" -force
+Move-Item "$env:TEMP\Fonts\*.ttf" "C:\Windows\Fonts\" -force
+
+Write-Host "Install Terminal-Icons if not installed"
 if (-not (Get-Module -Name "Terminal-Icons")) {
     Write-Host "Installing Terminal-Icons" 
     Install-Module Terminal-Icons -force }
-Move-Item $PROFILE "$($PROFILE)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
-Move-Item $PROFILE.AllUsersAllHosts "$($PROFILE.AllUsersAllHosts)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
-Copy-Item profile.ps1 $PROFILE.AllUsersAllHosts
-mkdir ~\.config\
+
+Write-Host "Install Profile"
+If (Test-Path -Path "$PROFILE") { Move-Item $PROFILE "$($PROFILE)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"}
+If (Test-Path -Path "$PROFILE.AllUsersAllHosts") { Move-Item $PROFILE.AllUsersAllHosts "$($PROFILE.AllUsersAllHosts)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"}
+Copy-Item profile.ps1 $PROFILE.AllUsersAllHosts -force
+If (! (Test-Path -Path "~\.config\")) {mkdir ~\.config}
 Copy-Item starship.toml ~\.config\starship.toml
